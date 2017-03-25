e03bbdfaf2280dabe339a3bd68a4198adc4e3bff
ScienisTmiaoT <1730402302@qq.com>
2017-03-25 21:12:55 +0800

    pair abs success

--- Interview Exercise/pair_abs.h
+++ Interview Exercise/pair_abs.h
@@ -29,33 +29,86 @@ void pairabs()
 		{
 			cin >> arr[i];
 		}
-		//sort(arr.begin(), arr.end());
+		sort(arr.begin(), arr.end());
 		long min_value = INTMAX;
 		long max_value = -1;
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = i + 1; j < n; j++)
-			{
-				long temp = abs(arr[j] - arr[i]);
-				if (temp < min_value)
-				{
-					min_count = 1;
-					min_value = temp;
-				}
-				else if(temp == min_value)
-					min_count++;
+		//for (int i = 0; i < n; i++)
+		//{
+		//	for (int j = i + 1; j < n; j++)
+		//	{
+		//		long temp = abs(arr[j] - arr[i]);
+		//		if (temp < min_value)
+		//		{
+		//			min_count = 1;
+		//			min_value = temp;
+		//		}
+		//		else if(temp == min_value)
+		//			min_count++;
 
-				if (temp > max_value)
-				{
-					max_count = 1;
-					max_value = temp;
-				}
-				else if (temp == max_value)
-					max_count++;
+		//		if (temp > max_value)
+		//		{
+		//			max_count = 1;
+		//			max_value = temp;
+		//		}
+		//		else if (temp == max_value)
+		//			max_count++;
+		//	}
+		//}
+
+		int q = 0;
+		int c = 1;
+		for (int i = 0; i < n - 1; i++)
+		{
+			long temp = abs(arr[i] - arr[i + 1]);
+			if (temp < min_value)
+			{
+				min_count = 1;
+				min_value = temp;
 			}
+			else if (temp == min_value)
+				min_count++;
+
+			if (arr[i] == arr[i + 1])
+			{
+				c++;
+			}
+			else
+			{
+				if (c != 1)
+				{
+					q += c * (c - 1) / 2;
+					c = 1;
+				}
+			}
+		}
+
+		if (q != 0)
+		{
+			if (c != 1)
+				min_count = q + c * (c - 1) / 2;
+			else
+				min_count = q;
+		}
+
+		int m = 1;
+		int s = 1;
+		if (arr[0] == arr[n - 1])
+		{
+			max_count = n * (n - 1) / 2;
+		}
+		else
+		{
+			for (int i = 1; i < n - 1; i++)
+			{
+				if (arr[i] == arr[0])
+					m++;
+				if (arr[i] == arr[n - 1])
+					s++;
+			}
+			max_count = m * s;
 		}
 		cout << min_count << " " << max_count << endl;
 	}
 }
 
-#endif // !
+#endif // !PAIR_ABS
