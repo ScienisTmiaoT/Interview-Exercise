7388d5c84a118bc33c866c6d9ba16526e481ae36
ScienisTmiaoT <1730402302@qq.com>
2017-03-31 11:45:03 +0800

    test

--- Interview Exercise/main.cpp
+++ Interview Exercise/main.cpp
@@ -7,6 +7,26 @@
 
 int main()
 {
-	pairabs();
+	int n = 0;
+	int m = 0;
+	vector<int> arr;
+	cin >> n;
+	for (int k = 0; k < n; k++)
+	{
+		cin >> m;
+		arr.push_back(m);
+	}
+	int i = n - 2;
+	for (; i >= 0; i--)
+	{
+		if (arr[i] > arr[i + 1])
+			break;
+	}
+	if (i <= 0)
+		cout << arr[0] << " ";
+	else
+		for (int j = 0; j <= i; j++)
+			cout << arr[j] << " ";
+	cout << endl;
 	return 0;
 }
