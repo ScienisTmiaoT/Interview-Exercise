b2be9f45679c1e6a60d209b3fe820e48a4ad946c
ScienisTmiaoT <1730402302@qq.com>
2017-04-08 21:33:28 +0800

    ms 2 test

--- Interview Exercise/main.cpp
+++ Interview Exercise/main.cpp
@@ -2,31 +2,73 @@
 ** Author: Marvin
 ** Date: 2017 / 03 / 25
 */
-#include "pair_abs.h"
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 
 int main()
 {
-	int n = 0;
-	int m = 0;
-	vector<int> arr;
+	//int n = 0;
+	//int m = 0;
+	//vector<int> arr;
+	//cin >> n;
+	//for (int k = 0; k < n; k++)
+	//{
+	//	cin >> m;
+	//	arr.push_back(m);
+	//}
+	//int i = n - 2;
+	//for (; i >= 0; i--)
+	//{
+	//	if (arr[i] > arr[i + 1])
+	//		break;
+	//}
+	//if (i <= 0)
+	//	cout << arr[0] << " ";
+	//else
+	//	for (int j = 0; j <= i; j++)
+	//		cout << arr[j] << " ";
+	//cout << endl;
+
+	long long n = 0;
+	long long q = 0;
+	long long time = 0;
+	long long t = 1;
 	cin >> n;
-	for (int k = 0; k < n; k++)
+	cin >> q;
+	long long i = 0;
+	if (2 * q >= n)
 	{
-		cin >> m;
-		arr.push_back(m);
+		cout << n << endl;
+		return 0;
 	}
-	int i = n - 2;
-	for (; i >= 0; i--)
+	else if (4 * q > n)
 	{
-		if (arr[i] > arr[i + 1])
-			break;
+		for (; i < 62; i++)
+		{
+			long long x = i * q * (t << i) + n;
+			long long y = n * (t << i);
+			if (x < y)
+				break;
+		}
 	}
-	if (i <= 0)
-		cout << arr[0] << " ";
 	else
-		for (int j = 0; j <= i; j++)
-			cout << arr[j] << " ";
-	cout << endl;
+	{
+		for (; i < 62; i++)
+		{
+			long long x = q * (t << (i + 1));
+			long long y = q * (t << (i + 2));
+			long long z = i * n;
+			long long w = (i + 1) * n;
+			if (x <= z && y > w)
+				break;
+		}
+	}
+	time = i * q + (n - 1) / (t << i) + 1;
+	cout << time << endl;
 	return 0;
 }
