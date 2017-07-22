979a0592195b98883f7e41096c2650800efb1e8d
ScienisTmiaoT <1730402302@qq.com>
2017-07-22 14:25:40 +0800

    codejam Round D

--- 84-Largest-Rectangle-in-Histogram/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "histogram.h"
-
-int main() {
-    Solution *s = new Solution();
-    std::vector<int> nums{2,1,5,6,2,3};
-    std::cout << s->largestRectangleArea(nums) << std::endl;
-    return 0;
-}
